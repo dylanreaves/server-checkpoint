8ac8a7e4a6d670ffb7e1582f9ccf94cac2d401f8
dylan reaves <dylanmilesreaves@gmail.com>
2026-06-26 16:39:23 -0400

    part 4 complete

--- app.js
+++ app.js
@@ -18,125 +18,191 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
-app.get("/api/plants", (req, res, next) => {
-    // Params: 
-    const params = req.params
-    console.log(params)
+// If this was placed at the bottom it will run the request first so nothing will be logged.
+app.use(logRequests)
 
-    // Queries :
-    const query = req.query
-    console.log(query)
+function logRequests(req, res, next) {
+    console.log(req.method, req.url)
+    next()
+}
 
-    const HasQuery = Object.keys(query).length > 0
-
-    if (HasQuery) {
-        const filtered_plants = plants.filter((plant) => {
-            return plant.type === query.type
-        })
-        return res.send(filtered_plants)
+// A great reason to have this middleware is to make sure any data coming into the server is valid so nothing breaks
+// If this middleware never calls next or never responds it will hang forever.
+function validPlant(req, res, next) {
+    if (req.body.name && req.body.type) {
+        next()
     } else {
-        return res.send(plants)
+        return res.status(400).send("Invalid plant: name or type is missing.")
     }
+}
 
-    return res.send(plants)
+app.get("/api/plants", (req, res, next) => {
+    try {
+        // req.params gets values that are part of the URL itself
+        // for example a request like '/api/plants/3' would make req.params.plantId be '3' 
+        // this is used when looking for one specific plant by its ID.
+
+        // req.query gets values placed after the ? in the URL and is used for filtering. 
+        // for example a request like '/api/plants?type=flower' would make req.eruery.type be "flower". 
+        // this could be used to filter and return plants whose type is "flower".
+
+        const params = req.params
+        const query = req.query
+        //console.log(params)
+        //console.log(query)
+
+        const HasQuery = Object.keys(query).length > 0
+
+        if (HasQuery) {
+            const filtered_plants = plants.filter((plant) => {
+                return plant.type === query.type
+            })
+            return res.send(filtered_plants)
+        } else {
+            return res.send(plants)
+        }
+    } catch(error) {
+        next(error)
+    }
 })
 
 app.get("/api/plants/:id", async (req, res, next) => {
-    // Because req.params is always a string we need to convert it into a number so that when
-    // actually comparing it we avoid a logic error where we are trying to compare a number to a string
-    const id = Number(req.params.id)
-    const found = plants.find((plant) => plant.id === id)
+    try {
+        // Because req.params is always a string we need to convert it into a number so that when
+        // actually comparing it we avoid a logic error where we are trying to compare a number to a string
+        const id = Number(req.params.id)
+        const found = plants.find((plant) => plant.id === id)
 
-    if (found) {
-        // If we were to remove await the function will not wait for the timeout to complete before returning
-        await new Promise((resolve) => setTimeout(resolve, 500))
-        return res.status(200).json(found) 
-    } else {
-        return res.status(404).send("No plant was found.")
+        if (found) {
+            // If we were to remove await the function will not wait for the timeout to complete before returning
+            await new Promise((resolve) => setTimeout(resolve, 500))
+            return res.status(200).json(found) 
+        } else {
+            return res.status(404).send("No plant was found.")
+        }
+    } catch(error) {
+        next(error)
     }
-
-    return res.send(plants)
 })
 
-app.post("/api/plants", (req, res, next) => {
-    const newPlant = {}
-    newPlant.id = nextId
-    Object.assign(newPlant, req.body)
-    plants.push(newPlant)
-    nextId++
-    return res.status(201).json(newPlant)
+app.post("/api/plants", validPlant, (req, res, next) => {
+    try {
+        const newPlant = {}
+        newPlant.id = nextId
+        Object.assign(newPlant, req.body)
+        plants.push(newPlant)
+        nextId++
+        return res.status(201).json(newPlant)
+    } catch(error) {
+        next(error)
+    }
 })
 
 app.patch("/api/plants/:id", (req, res, next) => {
-    // PATCH is made to merge new fields given in the body into an object instead of replacing it entirely.
-    // however in order to do this the programmer still need to decide the behavior for how it should work.
-    const id = Number(req.params.id)
-    const found = plants.find((plant) => plant.id === id)
-    
-    if (found) {
-        Object.assign(found, req.body)
-        return res.status(200).json(found) 
-    } else {
-        return res.status(404).send("No plant was found.")
+    try {
+        // PATCH is made to merge new fields given in the body into an object instead of replacing it entirely.
+        // however in order to do this the programmer still need to decide the behavior for how it should work.
+        const id = Number(req.params.id)
+        const found = plants.find((plant) => plant.id === id)
+        
+        if (found) {
+            Object.assign(found, req.body)
+            return res.status(200).json(found) 
+        } else {
+            return res.status(404).send("No plant was found.")
+        }
+    } catch(error) {
+        next(error)
     }
 })
 
 app.delete("/api/plants/:id", (req, res, next) => {
-    // PATCH is made to merge new fields given in the body into an object instead of replacing it entirely.
-    // however in order to do this the programmer still need to decide the behavior for how it should work.
-    const id = Number(req.params.id)
-    const found = plants.find((plant) => plant.id === id)
-    
-    if (found) {
-        plants.splice(plants.indexOf(found),1)
-        // 204 indicates the request went through and does not send any output 
-        return res.status(204).send("Removed a plant with ID: " + id)
-    } else {
-        // 404 indicates the server couldn't find the requested item
-        return res.status(404).send("No plant was found.")
+    try {
+        // PATCH is made to merge new fields given in the body into an object instead of replacing it entirely.
+        // however in order to do this the programmer still need to decide the behavior for how it should work.
+        const id = Number(req.params.id)
+        const found = plants.find((plant) => plant.id === id)
+        
+        if (found) {
+            plants.splice(plants.indexOf(found),1)
+            // 204 indicates the request went through and does not send any output 
+            return res.status(204).send("Removed a plant with ID: " + id)
+        } else {
+            // 404 indicates the server couldn't find the requested item
+            return res.status(404).send("No plant was found.")
+        }
+    } catch(error) {
+        next(error)
     }
 })
 
 app.get("/api/plants/:plantId/notes", (req, res, next) => {
-    // :plantId represents a dynamic parameter its a param because we need to locate which plant is being requested.
-    // notes isn't a parameter because it is the endpoint for which we get the note resources.
-    const plantId = Number(req.params.plantId)
-    const foundNotes = careNotes.filter((note) => note.plantId === plantId)
-    if (foundNotes.length > 0) {
-        return res.status(200).json(foundNotes)
-    } else {
-        return res.status(404).send("No care notes were found.")
+    try {
+        // :plantId represents a dynamic parameter its a param because we need to locate which plant is being requested.
+        // notes isn't a parameter because it is the endpoint for which we get the note resources.
+        const plantId = Number(req.params.plantId)
+        const foundNotes = careNotes.filter((note) => note.plantId === plantId)
+        if (foundNotes.length > 0) {
+            return res.status(200).json(foundNotes)
+        } else {
+            return res.status(404).send("No care notes were found.")
+        }
+    } catch(error) {
+        next(error)
     }
-
-    return res.send(plants)
 })
 
 app.post("/api/plants/:plantId/notes", (req, res, next) => {
-    const plantId = Number(req.params.plantId)
-    const newNote = {}
-    newNote.id = nextNoteId
-    newNote.plantId = plantId
-    Object.assign(newNote, req.body)
-    careNotes.push(newNote)
-    nextNoteId++
-    return res.status(201).json(newNote)
-})
-
-app.delete("/api/notes/:id", (req, res, next) => {
-    // This route begins with notes because we don't need to do anything with the plants route.
-    // We only need to find the note with the matching ID and remove it.
-    const id = Number(req.params.id)
-    const found = careNotes.find((note) => note.id === id)
-    
-    if (found) {
-        careNotes.splice(careNotes.indexOf(found),1)
-        // 204 indicates the request went through and does not send any output 
-        return res.status(204).send("Removed a note with ID: " + id)
-    } else {
-        // 404 indicates the server couldn't find the requested item
-        return res.status(404).send("No note was found.")
+    try {
+        const plantId = Number(req.params.plantId)
+        const newNote = {}
+        newNote.id = nextNoteId
+        newNote.plantId = plantId
+        Object.assign(newNote, req.body)
+        careNotes.push(newNote)
+        nextNoteId++
+        return res.status(201).json(newNote)
+    } catch(error) {
+        next(error)
     }
 })
 
+app.delete("/api/notes/:id", (req, res, next) => {
+    try {
+        // This route begins with notes because we don't need to do anything with the plants route.
+        // We only need to find the note with the matching ID and remove it.
+        const id = Number(req.params.id)
+        const found = careNotes.find((note) => note.id === id)
+        
+        if (found) {
+            careNotes.splice(careNotes.indexOf(found),1)
+            // 204 indicates the request went through and does not send any output 
+            return res.status(204).send("Removed a note with ID: " + id)
+        } else {
+            // 404 indicates the server couldn't find the requested item
+            return res.status(404).send("No note was found.")
+        }
+    } catch(error) {
+        next(error)
+    }
+})
+
+function handleError(req, res, next, error) {
+    res.status(500).send(error)
+    next()
+}
+
 
 app.listen(PORT, () => {console.log("Server running on port:", PORT)})
+
+
+// 1. When you stop and restart the server the plants and notes array are always initalized back to
+// the state of the array at the top.
+
+// 2. It really depends if the API isn't running locally on either then yes they would be the same any changes will be replicated.
+// if both are running it locally they would not be the same any changes made by one of them would not replicate to the other.
+
+// 3. The data would have to live somewhere else and not be hardcoded. 
+// Right now on every restart the data is always set to be whats in the array  
+// One way to do this is by using a database which we are going 
+// to be learning next week :)
