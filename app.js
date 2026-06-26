2cdb02c55e49348c8002146d0fd7cf8c4d9e4995
dylan reaves <dylanmilesreaves@gmail.com>
2026-06-26 14:15:07 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let plants = [
+  { id: 1, name: "Snake Plant", type: "Succulent", sunlight: "Low", watered: true },
+  { id: 2, name: "Pothos", type: "Vine", sunlight: "Medium", watered: false },
+  { id: 3, name: "Monstera", type: "Tropical", sunlight: "Medium", watered: true },
+  { id: 4, name: "Cactus", type: "Succulent", sunlight: "High", watered: false },
+];
+
+let nextId = 5;
